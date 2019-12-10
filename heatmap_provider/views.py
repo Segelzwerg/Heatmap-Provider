bd1c0a89002593929a633d8291eea00cdfb7e375
Marcel <marcel.haas@hhu.de>
2019-12-11 00:14:20 +0100

    ADD: skeleton put method
    Signed-off-by: Marcel <marcel.haas@hhu.de>

--- heatmap_provider/views.py
+++ heatmap_provider/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import FileUploadParser
+from rest_framework.views import APIView
 
 
 def index(request):
     return HttpResponse("Welcome to Heatmap Provider.")
+
+
+@APIView
+@parser_classes([FileUploadParser])
+def put(request, filename, format=None):
+    file_object = request.data['file']
+    return HttpResponse("File Uploaded")
