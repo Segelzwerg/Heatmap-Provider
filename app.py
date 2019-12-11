5c59bd03fdfa72219b1c30f9a4e5e2806ecf6386
Marcel <marcel.haas@hhu.de>
2019-12-11 21:23:22 +0100

    ADD: puts method
    Signed-off-by: Marcel <marcel.haas@hhu.de>

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/upload', methods=['GET', 'POST', 'PUT'])
+def upload_file():
+    r = request
+    if request.method == 'PUT':
+        # TODO: save body
+        return 'file uploaded successfully'
+    elif request.method == 'GET':
+        return "Please send a gpx file."
 
 
 if __name__ == '__main__':
