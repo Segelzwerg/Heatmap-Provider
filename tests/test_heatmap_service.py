a33a61cf44055d4b44a935b0924e1357a8537da6
Segelzwerg <marcel.haas@hhu.de>
2019-12-18 16:27:48 +0100

    ADD: test for heatmap service including resources
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

---
+++ tests/test_heatmap_service.py
@@ -0,0 +1,12 @@
+import os
+from unittest import TestCase
+
+from App.heatmap_service import heatmap_service
+
+
+class Test(TestCase):
+    def test_heatmap_service(self):
+        gpx = "resources/2019-09-13_93366270_Zum_Apfelbaum.gpx"
+        path = os.path.dirname(gpx)
+        result = heatmap_service("1", path)
+        assert result == "Generated"
