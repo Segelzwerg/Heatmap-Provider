c280d03c58dc9c820fcb998baba92f250c70d101
Segelzwerg <marcel.haas@hhu.de>
2019-12-18 19:47:24 +0100

    ADD: resources path getter for different platforms
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- tests/test_heatmap_service.py
+++ tests/test_heatmap_service.py
@@ -3,9 +3,21 @@ import os
 from App.heatmap_service import heatmap_service
 
 
+def getResourcePath():
+    path = "resources/"
+    abs_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources/")
+    if os.path.isdir(path):
+        return path
+    elif os.path.isdir("test/resources/"):
+        return "/test/resources/"
+    elif abs_path:
+        return abs_path
+
+    raise IOError("Resources not found")
+
+
 def test_heatmap_service():
-    gpx = "resources/2019-09-13_93366270_Zum_Apfelbaum.gpx"
-    path = os.path.dirname(gpx)
+    path = getResourcePath()
     result = heatmap_service("1", path)
     assert result == "Generated"
     assert os.path.isfile("1.png")
