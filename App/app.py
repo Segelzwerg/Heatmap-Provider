0816d3e6fa0c46b0f4d9100cb85077165dbd0a4a
Segelzwerg <marcel.haas@hhu.de>
2020-02-10 08:27:54 +0100

    FIX: port binding
    Signed-off-by: Segelzwerg <marcel.haas@hhu.de>

--- App/app.py
+++ App/app.py
@@ -80,4 +80,5 @@ if __name__ == '__main__':
         print(e)
         print("Already clean instance.")
     print("Heatmap Provider is online.")
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
